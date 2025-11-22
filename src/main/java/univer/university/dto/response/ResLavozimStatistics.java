5977d29a0955aa53d26f6651cd0eea61ef1ac558
Muhammad <muhammadnazirqulov1@gmail.com>
2025-11-22 15:00:10 +0500

    3 chi commit

--- src/main/java/univer/university/dto/response/ResLavozimStatistics.java
+++ src/main/java/univer/university/dto/response/ResLavozimStatistics.java
@@ -1,13 +1,13 @@
-//package univer.university.dto.response;
-//
-//import lombok.*;
-//
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//public class ResLavozimStatistics {
-//    private String name;
-//    private Integer totalEmployees;
-//}
+package univer.university.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResLavozimStatistics {
+    private String name;
+    private Integer totalEmployees;
+}
