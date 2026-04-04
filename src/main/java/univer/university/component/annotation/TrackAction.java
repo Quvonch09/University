e4e719d5f13a5aaf94e3f6dba8aecf315a2de673
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2026-04-04 13:43:01 +0500

    kamchilik tugrilandi va TrackAction annotatsiya qo'shildi

---
+++ src/main/java/univer/university/component/annotation/TrackAction.java
@@ -0,0 +1,14 @@
+package univer.university.component.annotation;
+
+import univer.university.entity.enums.ActionType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TrackAction {
+    ActionType value();
+}
